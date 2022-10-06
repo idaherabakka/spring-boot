78ca981789a6ad155603fee9b98b6649f8aecea1
idaher <xek010@uib.no>
2022-10-06 21:29:25 +0200

    Initial commit

---
+++ demo/demo/src/main/java/com/example/restservice/Greeting.java
@@ -0,0 +1,23 @@
+package com.example.restservice;
+
+import com.example.springboot.Application;
+import org.springframework.boot.SpringApplication;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
